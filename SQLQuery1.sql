9a132addef34a4bf81ae5aa0c9f2439ee91944e0
Aksh58 <112678445+Aksh58@users.noreply.github.com>
2022-09-02 13:27:57 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,12 +3,12 @@ use client
 
 create table client_tb1
 (
- ClientID  int Primary key,
- Cname Varchar(40) NOT NULL,
- Address   VARCHAR(30), 
- Email  Varchar(30) UNIQUE, 
- Phone varchar(10),
- Business  Varchar(20) NOT NULL,
+ CLIENTID  INT PRIMARY KEY,
+ CNAME VARCHAR(40) NOT NULL,
+ ADDRESS   VARCHAR(30), 
+ EMAIL  VARCHAR(30) UNIQUE, 
+ PHONE VARCHAR(10),
+ BUSINESS  VARCHAR(20) NOT NULL,
  )
  insert into client_tb1 values(1004,'Lawful Corp','Chennai','justice@lawful.com','9210342219','Professional')
  select *from client_tb1
